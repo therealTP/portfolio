2deefa7ea857d5ad83d73fa7798878ac565ea035
Tyler Palmer <mr.tylerpalmer@gmail.com>
2016-01-23 23:39:25 -0700

    Re-did layout with manual jquery & css. no more fullpage.js dependency. Areplaced arrows, added deck-style animation only from left

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,163 @@
 $(document).ready(function(){
-  $('#mainContent').fullpage({
-      sectionSelector: '#mainContent',
-      slideSelector: '.slide-section',
-      loopHorizontal: false,
-      anchors: ['main-content'],
-      onSlideLeave: function(anchorLink, index, slideIndex, nextSlideIndex){
-        // remove active class from any menu item
-        $('#mainNav a').removeClass('active-menu');
-        // add active class to correct menu item
-        if (nextSlideIndex === 'right') {
-          $('#mainNav li:nth-Child(' + (slideIndex + 1) + ') a').addClass('active-menu');
-        } else if (nextSlideIndex === 'left') {
-          $('#mainNav li:nth-Child(' + (slideIndex - 1) + ') a').addClass('active-menu');
-        }
-      }
+
+  function nextSlide() {
+    var $activeSlide = $('.active-section');
+    var activeSlideId = $activeSlide.attr('id');
+    console.log(activeSlideId);
+    if (activeSlideId !== 'contact') { // can't go right of contact slide
+      var $nextSlide = $activeSlide.next('.slide-section');
+      var nextSlideId = $nextSlide.attr('id');
+      $('.active-section').removeClass('active-section');
+      console.log(nextSlideId);
+      // at same time:
+      // animate active slide to left, w/ duration/easing settings
+      $activeSlide.animate({
+        left: "100vw",
+        right: "-100vw"
+      }, {
+        start: function() {
+          $('.active-section').removeClass('active-section'); // make all sections inactive
+        },
+        duration: 1200, // custom for each. get options by id
+        easing: 'swing'
+      });
+
+      $nextSlide.animate({
+        left: 0, // these are same for all: need to bring next slide into view
+        right: 0
+      }, {
+        start: function() {
+          $(this).addClass('active-section'); // when done, make new section active
+        },
+        duration: 1000,
+        easing: 'swing'
+      });
+    }
+  }
+
+  function prevSlide() {
+    var $activeSlide = $('.active-section');
+    var activeSlideId = $activeSlide.attr('id');
+    if (activeSlideId !== "home") { // can't go left of home slide
+      var $prevSlide = $activeSlide.prev('.slide-section');
+      var prevSlideId = $prevSlide.attr('id');
+      // at same time:
+      // animate active slide to left, w/ duration/easing settings
+      $activeSlide.animate({
+        left: "100vw",
+        right: "-100vw"
+      }, {
+        start: function() {
+          $('.active-section').removeClass('active-section');
+        },
+        duration: 1200, // custom for each. get options by id
+        easing: 'swing'
+      });
+
+      $prevSlide.animate({
+        left: 0, // these are same for all: need to bring next slide into view
+        right: 0
+      }, {
+        done: function() {
+          $(this).addClass('active-section');
+        },
+        duration: 800,
+        easing: 'swing'
+      });
+    }
+  }
+
+  $('a.section-link').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('data-target');
+    goToCard(id);
   });
 
+  function goToCard(targetId) {
+    var $activeSlide = $('.active-section');
+    var $targetSlide = $('#' + targetId);
+    $activeSlide.animate({
+      left: "100vw",
+      right: "-100vw"
+      }, {
+      start: function() {
+        $('.active-section').removeClass('active-section');
+      },
+      duration: 600, // custom for each. get options by id
+      easing: 'swing'
+    });
+    $targetSlide.animate({
+      left: 0,
+      right: 0
+    }, {
+      start: function() {$targetSlide.addClass('active-section');}
+    });
+    // $activeSlide.animate({
+    //   left: "100vw",
+    //   right: "-100vw"
+    //   }, {
+    //   start: function() {
+    //     $('.active-section').removeClass('active-section');
+    //   },
+    //   duration: 600, // custom for each. get options by id
+    //   easing: 'swing',
+    //   done: function() {
+    //     $targetSlide.addClass('active-section');
+    //     $targetSlide.animate({
+    //       left: 0,
+    //       right: 0
+    //     });
+    //   }
+    // });
+  }
+    // else {
+    // $activeSlide.animate({
+    //   left: "100vw",
+    //   right: "-100vw"
+    //   }, {
+    //   start: function() {
+    //     $('.active-section').removeClass('active-section');
+    //   },
+    //   duration: 600, // custom for each. get options by id
+    //   easing: 'swing'
+    // });
+    // $targetSlide.animate({
+    //   left: 0,
+    //   right: 0
+    // });
+    // }
+  // }
+
+  // event listeners for forward/back arrows
+  $('a.next-slide').on('click', nextSlide);
+  $('a.prev-slide').on('click', prevSlide);
+
   // add event listener for mousewheel
-  $('html').bind('mousewheel', function(e){
-      if(e.originalEvent.wheelDelta /120 > 0) {
-        $.fn.fullpage.moveSlideLeft();
+  $('body').bind('mousewheel DOMMouseScroll', function(e){
+    // if
+    if ($('.slide-section:animated').length > 0) {
+      e.preventDefault();
+    } else {
+      if(e.originalEvent.wheelDelta /120 > 0 || e.originalEvent.detail > 0) {
+        prevSlide();
       }
       else{
-        $.fn.fullpage.moveSlideRight();
+        nextSlide();
       }
+    }
   });
+
+  // event listener for left/right keypresses
+  $('body').on('keydown', function(e) {
+    if ($('.slide-section:animated').length > 0) {
+      e.preventDefault();
+    } else {
+      if (e.which === 37) {
+        prevSlide();
+      } else if (e.which === 39) {
+        nextSlide();
+      }
+    }
+  });
+
 });
